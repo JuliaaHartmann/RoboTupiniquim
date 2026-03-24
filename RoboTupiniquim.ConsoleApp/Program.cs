f7345baef33cb97efed1870e60fb8f33510c8df2
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-03-24 17:26:39 -0300

    first commit

---
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Reflection.PortableExecutable;
+
+namespace RoboTupiniquim.ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("-------------------------");
+        Console.WriteLine("-----Robô Tupiniquim-----");
+        Console.WriteLine("-------------------------");
+
+    }
+
+}
