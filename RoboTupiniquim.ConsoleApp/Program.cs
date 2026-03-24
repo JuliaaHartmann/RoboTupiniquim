c4feb1dac02286e6525dbb0e479868d80f2f55d2
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-03-24 17:30:38 -0300

    Implementada lógica de movimentação e direção do robô

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -11,6 +11,76 @@ class Program
         Console.WriteLine("-----Robô Tupiniquim-----");
         Console.WriteLine("-------------------------");
 
+        // Solicitar ao usuário o valor de X (int)
+        Console.Write("Digite o valor da posição X: ");
+        int valorX = Convert.ToInt32(Console.ReadLine());
+
+        // Solicitar ao usuário o valor de Y (int)
+        Console.Write("Digite o valor da posição Y: ");
+        int valorY = Convert.ToInt32(Console.ReadLine());
+
+        // Solicitar ao usuário a direção (string)
+        Console.Write("Digite a direção: ");
+        string? direcao = Console.ReadLine()?.ToUpper();
+
+        //Solicitar ao usuário uma série de instruções (E, D, M) que o robô deve seguir
+        Console.WriteLine("Digite a sequência de comandos: ");
+        string? comandos = Console.ReadLine()?.ToUpper(); // ['E', 'M', 'E', 'M', 'E', 'M', 'E', 'M', 'M']
+
+        foreach (char comando in comandos)
+        {
+            //Mudar direção para esquerda
+            if (comando == 'E')
+            {
+                if (direcao == "N")
+                    direcao = "O";
+
+                else if (direcao == "O")
+                    direcao = "S";
+
+                else if (direcao == "S")
+                    direcao = "L";
+
+                else
+                    direcao = "N";
+            }
+
+            //Mudar direção para D
+            if (comando == 'D')
+            {
+                if (direcao == "N")
+                    direcao = "L";
+
+                else if (direcao == "L")
+                    direcao = "S";
+
+                else if (direcao == "S")
+                    direcao = "O";
+
+                else 
+                    direcao = "N";                    
+            
+            }
+
+            //Mover "objeto"
+            if (comando == 'M')
+            {
+                if (direcao == "N")
+                    valorY += 1;
+
+                else if (direcao == "O")
+                    valorX -= 1;
+
+                else if (direcao == "S") 
+                    valorY -= 1;
+
+                else 
+                    valorX += 1;
+            }
+
+        }
+
+        Console.WriteLine($"Posição final: {valorX} {valorY} {direcao}");
     }
 
 }
