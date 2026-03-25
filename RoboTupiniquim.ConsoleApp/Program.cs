7233f0f3ccae7c63b214001b9aabe7b5ae9ecf36
Júlia Hartmann <juliahartmann@outlook.com.br>
2026-03-25 15:52:55 -0300

    Ajustes na lógica de tratamento de comandos e retirados os comentários

--- RoboTupiniquim.ConsoleApp/Program.cs
+++ RoboTupiniquim.ConsoleApp/Program.cs
@@ -7,75 +7,47 @@ class Program
 {
     static void Main(string[] args)
     {
+
         Console.WriteLine("-------------------------");
         Console.WriteLine("-----Robô Tupiniquim-----");
         Console.WriteLine("-------------------------");
 
-        // Solicitar ao usuário o valor de X (int)
         Console.Write("Digite o valor da posição X: ");
         int valorX = Convert.ToInt32(Console.ReadLine());
 
-        // Solicitar ao usuário o valor de Y (int)
         Console.Write("Digite o valor da posição Y: ");
         int valorY = Convert.ToInt32(Console.ReadLine());
 
-        // Solicitar ao usuário a direção (string)
         Console.Write("Digite a direção: ");
         string? direcao = Console.ReadLine()?.ToUpper();
 
-        //Solicitar ao usuário uma série de instruções (E, D, M) que o robô deve seguir
         Console.WriteLine("Digite a sequência de comandos: ");
-        string? comandos = Console.ReadLine()?.ToUpper(); // ['E', 'M', 'E', 'M', 'E', 'M', 'E', 'M', 'M']
+        string? comandos = Console.ReadLine()?.ToUpper();
 
         foreach (char comando in comandos)
         {
-            //Mudar direção para esquerda
             if (comando == 'E')
             {
-                if (direcao == "N")
-                    direcao = "O";
-
-                else if (direcao == "O")
-                    direcao = "S";
-
-                else if (direcao == "S")
-                    direcao = "L";
-
-                else
-                    direcao = "N";
+                if (direcao == "N") direcao = "O";
+                else if (direcao == "O") direcao = "S";
+                else if (direcao == "S") direcao = "L";
+                else if (direcao == "L") direcao = "N";
             }
 
-            //Mudar direção para D
             if (comando == 'D')
             {
-                if (direcao == "N")
-                    direcao = "L";
-
-                else if (direcao == "L")
-                    direcao = "S";
-
-                else if (direcao == "S")
-                    direcao = "O";
-
-                else 
-                    direcao = "N";                    
-            
+                if (direcao == "N") direcao = "L";
+                else if (direcao == "L") direcao = "S";
+                else if (direcao == "S") direcao = "O";
+                else if (direcao == "O") direcao = "N";
             }
 
-            //Mover "objeto"
             if (comando == 'M')
             {
-                if (direcao == "N")
-                    valorY += 1;
-
-                else if (direcao == "O")
-                    valorX -= 1;
-
-                else if (direcao == "S") 
-                    valorY -= 1;
-
-                else 
-                    valorX += 1;
+                if (direcao == "N") valorY++;
+                else if (direcao == "O") valorX--;
+                else if (direcao == "S") valorY--;
+                else if (direcao == "L") valorX++;
             }
 
         }
